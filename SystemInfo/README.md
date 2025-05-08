c9d574b9b79399ea19c5ef7f7b719ec8b2610f24
Youssef Shaaban <yousafsh351@gmail.com>
2025-05-08 20:16:29 +0300

    Upload File

---
+++ SystemInfo/README.md
@@ -0,0 +1,15 @@
+# System Info
+
+@by- youcefshaaban
+
+Show Your System Info
+
+Only On Windows
+
+## How It Works?
+
+1. It gets your system info by `systeminfo` command
+
+## Instructions
+
+Open The Bat File And Then Wait for it.
---
+++ SystemInfo/SystemInfo.bat
@@ -0,0 +1,22 @@
+@echo off
+cd "C:\Windows\System32"
+chsp 65001
+cls
+
+echo  ___  _  _  ___  ____  ____  __  __    ____  _  _  ____  _____
+echo / __)( \/ )/ __)(_  _)( ___)(  \/  )  (_  _)( \( )( ___)(  _  )
+echo \__ \ \  / \__ \  )(   )__)  )    (    _)(_  )  (  )__)  )(_)(
+echo (___/ (__) (___/ (__) (____)(_/\/\_)  (____)(_)\_)(__)  (_____) 
+echo 																	by- @youcefshaaban
+
+echo.
+echo.
+echo.
+
+echo Please Wait...
+
+timeout /t 5 >nul
+
+systeminfo
+
+pause
